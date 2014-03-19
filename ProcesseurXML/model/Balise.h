4c314412c892299397364ac428cde9cf29303e7f
Gourgeon Sylvain <sgourgeon@if501-219-03.insa-lyon.fr>
2014-03-19 08:19:04 +0100

    Classes pour la balise

---
+++ ProcesseurXML/model/Balise.h
@@ -0,0 +1,12 @@
+typedef enum TypeBalise {OUVRANTE, FERMANTE, ORPHELINE} TypeBalise;
+
+class Balise
+{
+public:
+	Balise(char *);
+	~Balise();
+private:
+	TypeBalise typeBalise;
+	//Nom nomBalise;
+	//Attribut attributs;
+};
