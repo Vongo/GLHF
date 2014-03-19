25ba356cb287cab36153ad18642bd7a02dc7d409
Vongo <adelmarre@vongo.eu>
2014-03-19 08:23:59 +0100

    Cosmetics

--- ProcesseurXML/model/Balise.h
+++ ProcesseurXML/model/Balise.h
@@ -3,10 +3,10 @@ typedef enum TypeBalise {OUVRANTE, FERMANTE, ORPHELINE} TypeBalise;
 class Balise
 {
 public:
-	Balise(char *);
-	~Balise();
+    Balise(char *);
+    ~Balise();
 private:
-	TypeBalise typeBalise;
-	//Nom nomBalise;
-	//Attribut attributs;
+    TypeBalise typeBalise;
+    //Nom nomBalise;
+    //Attribut attributs;
 };
