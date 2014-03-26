a7d0fbee87f26d287f94a9a63cd7749dc6705aaf
Vongo <adelmarre@vongo.eu>
2014-03-26 12:06:18 +0100

    Standard main formating for usual args.

--- ProcesseurXML/main.cpp
+++ ProcesseurXML/main.cpp
@@ -6,24 +6,75 @@
 using namespace std;
 
 int xmlparse(Document **);
+int xmltransformation(const char *, const char *);
+int xmlvalidation(const char *, const char *);
+
+// TEMP //
+
+int xmltransformation(const char *, const char *)
+{
+    return 1;
+}
+int xmlvalidation(const char *, const char *)
+{
+    return 1;
+}
+
+// END TEMP //
 
 int main(int argc, char const *argv[])
 {
     Document **doc;
     extern FILE *xmlin;
-    int retour = 1;
-    if (argc == 2 && argv[0] == "-p")
+    int retour = 0;
+    if (argc == 2)
     {
-        FILE *fid = fopen(argv[1], "r");
-        if (!fid)
+        switch (argv[1][1])
         {
-            cout << "ACHTUNG OMFGWTF" << endl;
-            return 1;
+        case 'h':
+            cout << "CECI EST LE MANUEL OLOL" << endl;
+            break;
+        default:
+            cout << "Argument non reconnu : " << argv[1][1] << endl;
         }
-        xmlin = fid;
-        retour = xmlparse(doc);
-        fclose(fid);
     }
+    else if (argc == 3)
+    {
+        if (argv[0] == "-p")
+        {
+            cout << ">> PARSING <<" << endl;
+            FILE *fid = fopen(argv[2], "r");
+            if (!fid)
+            {
+                cout << "ACHTUNG OMFGWTF" << endl;
+                return 1;
+            }
+            xmlin = fid;
+            retour = xmlparse(doc);
+            fclose(fid);
+        }
+    }
+    else if (argc == 4)
+    {
+        switch (argv[1][1])
+        {
+        case 'v':
+            cout << ">> VALIDATION <<" << endl;
+            retour = xmlvalidation(argv[2], argv[3]);
+            break;
+        case 't':
+            cout << ">> TRANSFORMATION <<" << endl;
+            retour = xmltransformation(argv[2], argv[3]);
+            break;
+        default:
+            cout << "Argument non reconnu : " << argv[1][1] << endl;
+        }
+    }
+    else
+    {
+        cout << "Mauvais format d'entree" << endl;
+    }
+
     if (!retour)
     {
         cout << "Entrée standard reconnue" << endl;
