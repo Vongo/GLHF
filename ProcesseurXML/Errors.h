3a7fa2e3f613f9d6409c572d3435908a6b9e2a4e
leovetter <vetter.leo@gmail.com>
2014-04-03 01:36:52 +0200

    edit

--- ProcesseurXML/Errors.h
+++ ProcesseurXML/Errors.h
@@ -8,18 +8,10 @@ static const char *TOO_MANY_ARGUMENTS = "TODO Too many arguments\n";
 static const char *UNPROVIDED_ARGUMENT_P = "You must provide an argument to the command -p\n";
 static const char *UNPROVIDED_ARGUMENT_T = "You must provide two arguments to the command -t: an xml file and an xsl file\n";
 static const char *UNPROVIDED_ARGUMENT_V = "You must provide two arguments to the command -v: an xml file and an xsd file\n";
-static const char *BAD_MARKUP_1 = "Non matching element names title and titre\n";
-static const char *BAD_MARKUP_2 = "Non matching element namespaces ns and badns\n";
-static const char *BAD_MARKUP_3 = "Non matching element names catalog and catalogue\n";
-static const char *BAD_MARKUP_4 = "Non matching element namespaces ns and badns\nNon matching element names catalog and catalogue\n";
-static const char *LEXICAL_ERROR = "Lexical Error (character ?)\nLexical Error (character !)\nsyntax error\nNo root markup\n";
-static const char *MISSING_MARKUP = "Non matching element names artist and cd\nNon matching element names cd and catalog\nsyntax error\nNo root markup\n";
-static const char *SYNTAX_ERROR = "Parsage d'un fichier correct au niveau lexical mais dont la syntaxe n'est pas bonne.l\n";
 static const char *CANNOT_OPEN_XML1 = "Unable to open does_not_exist.xml\n";
 static const char *CANNOT_OPEN_XML2 = "Unable to open file does_not_exist.xml\n";
 static const char *CANNOT_OPEN_XSL = "Unable to open file does_not_exist.xsl\n";
 static const char *CANNOT_OPEN_XSD = "Unable to open file does_not_exist.xsd\n";
-static const char *NO_ROOT_ELEMENT = "No root markup\n";
 
 
 
